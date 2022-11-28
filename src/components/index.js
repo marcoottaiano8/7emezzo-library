b783edd1b1f90c3a3e9a2cfa27b8c515d2fb852e
marcoottaiano <marco.ottaiano@beije.it>
2022-11-28 16:05:59 +0100

    no message

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import CustomButton from "./CustomButton";
-import CustomInputBox from "./CustomInputBox"
-import Utils from "./utils/utils"
-import Login from "./screens/Login"
+import CustomInputBox from "./CustomInputBox";
+import Utils from "./utils/utils";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
 
-export { CustomButton, CustomInputBox, Login, Utils};
+export { CustomButton, CustomInputBox, Login, Utils, Signup };
